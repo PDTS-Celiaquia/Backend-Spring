aa85f2d217c6782399e0b9cca8b9189511481221
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2021-07-17 19:26:59 -0300

    remove final and GlobalExceptionHandler

--- src/main/java/edu/fi/mdp/celiacos/controller/UsuarioController.java
+++ src/main/java/edu/fi/mdp/celiacos/controller/UsuarioController.java
@@ -1,7 +1,6 @@
 package edu.fi.mdp.celiacos.controller;
 
 import edu.fi.mdp.celiacos.auth.AuthorityEnum;
-import edu.fi.mdp.celiacos.auth.Usuario;
 import edu.fi.mdp.celiacos.exception.UnauthorizedException;
 import edu.fi.mdp.celiacos.model.dto.LoginDTO;
 import edu.fi.mdp.celiacos.model.dto.UsuarioWebDTO;
@@ -9,8 +8,6 @@ import edu.fi.mdp.celiacos.service.UsuarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +17,8 @@ import javax.validation.Valid;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/usuarioWeb")
-final class UsuarioController {
+@RequestMapping("/usuario")
+public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping("/register")
