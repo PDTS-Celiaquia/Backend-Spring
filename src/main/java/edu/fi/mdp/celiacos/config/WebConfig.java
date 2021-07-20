200fa8960b696b543022c13fa382a6fb582b2147
augustomtt <65096407+augustomtt@users.noreply.github.com>
2021-07-20 11:26:02 -0300

    arreglado problema para conectar el front a heroku

--- src/main/java/edu/fi/mdp/celiacos/config/WebConfig.java
+++ src/main/java/edu/fi/mdp/celiacos/config/WebConfig.java
@@ -3,13 +3,14 @@ package edu.fi.mdp.celiacos.config;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
 
     @Override
@@ -24,5 +25,15 @@ public class MvcConfig implements WebMvcConfigurer {
         if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
 
         registry.addResourceHandler("/public/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
+    }
+
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*")
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods("");
     }
 }
