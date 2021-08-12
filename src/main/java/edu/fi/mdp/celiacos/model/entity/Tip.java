3792d4c2f96741ddc3cc09ae90f86810def8dba5
augustomtt <65096407+augustomtt@users.noreply.github.com>
2021-08-12 14:50:32 -0300

    Create Tip Entity
    Create Tip Entity - First implementation

---
+++ src/main/java/edu/fi/mdp/celiacos/model/entity/Tip.java
@@ -0,0 +1,38 @@
+package edu.fi.mdp.celiacos.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tips")
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Tip {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(name = "titulo")
+    private String titulo;
+
+    @Size(max = 5000)
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Size(max = 8192)
+    @Column(name = "enlace")
+    private String enlace;
+
+}
